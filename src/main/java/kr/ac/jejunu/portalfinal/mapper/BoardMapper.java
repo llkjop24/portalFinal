7772c94ef3c6624e63c8898cf54f431fc0e7f5df
jiwon <llkjop24@gmail.com>
2018-06-14 19:15:50 +0900

    reset

--- src/main/java/kr/ac/jejunu/portalfinal/mapper/BoardMapper.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.jejunu.portalfinal.mapper;
-
-import kr.ac.jejunu.portalfinal.domain.BoardVO;
-import org.apache.ibatis.annotations.Param;
-
-public interface BoardMapper {
-
-    public void boardInsert(BoardVO board) throws Exception;
-}
