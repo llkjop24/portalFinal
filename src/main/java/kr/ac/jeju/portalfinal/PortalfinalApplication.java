8362cf44e89435b59be6ec6d891e691bcd578538
jiwon <llkjop24@gmail.com>
2018-06-14 19:49:26 +0900

    controller test

---
+++ src/main/java/kr/ac/jeju/portalfinal/PortalfinalApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.jeju.portalfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalfinalApplication.class, args);
+    }
+}
---
+++ src/main/java/kr/ac/jeju/portalfinal/TestController.java
@@ -0,0 +1,13 @@
+package kr.ac.jeju.portalfinal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
+}
